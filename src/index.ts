ff353e37148c432804a0ebfe50bf5d2fee5dedd3
teawithsand <teawithsand@gmail.com>
2022-02-22 14:27:07 +0000

    Exported fullscreen hooks

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { Item, ItemSource } from "./image"
 export { Gallery, GalleryProps, GalleryMode, GalleryConfig, GalleryCallbacks, GalleryState } from "./gallery"
+export { useFullscreen, useSoftFullscreen } from "./useFullscreen"
